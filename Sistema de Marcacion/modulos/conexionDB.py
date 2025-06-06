e3f08585aa2f0660a659f7661ee77d45d59acc4e
LuisMM727 <luisluismm727@gmail.com>
2025-06-06 20:34:44 -0300

    Se modifico

--- Sistema de Marcacion/modulos/conexionDB.py
+++ Sistema de Marcacion/modulos/conexionDB.py
@@ -1,3 +1,4 @@
+#Funcion para conectar con la BD
 import pymysql
 def obtener_conexion():
     return pymysql.connect(host='localhost', user='root', password='', db='sistemamc', cursorclass=pymysql.cursors.DictCursor)
